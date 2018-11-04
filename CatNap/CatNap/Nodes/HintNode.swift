c12a00fcc5b162471e91aa89cff8e3b247538328
hongqima <hongqima@yangqianguan.com>
2018-11-05 02:18:12 +0800

    CatNap: hint shape change fillColor on tap

--- CatNap/CatNap/Nodes/HintNode.swift
+++ CatNap/CatNap/Nodes/HintNode.swift
@@ -9,7 +9,9 @@
 import SpriteKit
 
 class HintNode: SKSpriteNode {
-  var arrowPath: CGPath = {
+  private let randomColor: [SKColor] = [.red, .gray, .yellow, .green, .blue, .orange]
+  
+  private var arrowPath: CGPath = {
     let bezierPath = UIBezierPath()
     bezierPath.move(to: CGPoint(x: 0.5, y: 65.69))
     bezierPath.addLine(to: CGPoint(x: 74.99, y: 1.5))
@@ -22,11 +24,29 @@ class HintNode: SKSpriteNode {
     bezierPath.close()
     return bezierPath.cgPath
   }()
+  
+  private lazy var shape: SKShapeNode = {
+    let shape = SKShapeNode(path: arrowPath)
+    shape.strokeColor = .gray
+    shape.lineWidth = 4
+    shape.fillColor = .white
+    shape.fillTexture = SKTexture(imageNamed: "wood_tinted")
+    shape.alpha = 0.8
+    return shape
+  }()
+  
+  
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesEnded(touches, with: event)
+    interact()
+  }
 }
 
 extension HintNode: EventListenerNode {
   func didMoveToScene() {
     color = .clear
+    isUserInteractionEnabled = true
     
 //    let shape = SKShapeNode(rectOf: size, cornerRadius: 20)
 //    shape.strokeColor = .red
@@ -34,17 +54,18 @@ extension HintNode: EventListenerNode {
 //    shape.glowWidth = 5
 //    shape.fillColor = .white
 //    addChild(shape)
-    let shape = SKShapeNode(path: arrowPath)
-    shape.strokeColor = .gray
-    shape.lineWidth = 4
-    shape.fillColor = .white
-    shape.fillTexture = SKTexture(imageNamed: "wood_tinted")
-    shape.alpha = 0.8
+    
     addChild(shape)
     
     let move = SKAction.moveBy(x: -40, y: 0, duration: 1.0)
     shape.run(SKAction.repeat(SKAction.sequence([move, move.reversed()]), count: 3)) {
       self.removeFromParent()
     }
+  }
+}
+
+extension HintNode: InteractiveNode {
+  func interact() {
+    shape.fillColor = randomColor.randomElement()!
   }
 }
